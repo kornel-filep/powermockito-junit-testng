3494a52f5b5b93436e823cc10279c7e245eec1e7
Kornel Filep <kornel_filep@epam.com>
2020-06-04 14:16:09 +0200

    added a junit config example

--- src/test/java/com/asd/MomTest.java
+++ src/test/java/com/asd/MomTest.java
@@ -1,17 +1,19 @@
 package com.asd;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.testng.PowerMockTestCase;
-import org.testng.annotations.Test;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+@RunWith(PowerMockRunner.class)
 @PrepareForTest({Mom.class})
-class MomTest extends PowerMockTestCase {
+public class MomTest {
 
     Mom mom = new Mom();
 
     @Test
-    void testAuch() throws Exception {
+    public void testAuch() throws Exception {
         Child mockChild = PowerMockito.mock(Child.class);
         PowerMockito.whenNew(Child.class).withAnyArguments().thenReturn(mockChild);
         PowerMockito.doAnswer(c -> {
